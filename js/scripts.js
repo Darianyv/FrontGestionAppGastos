7309f25a27c5069876ec9201396428248d8a4ea9
Yoneibi Neira <yoneibi_10@gmail.com>
2024-10-22 19:35:29 -0500

    Fronend Api

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Mostrar el formulario de registro y ocultar el de inicio de sesión
+document.getElementById("showRegisterForm").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("loginFormContainer").classList.add("d-none");
+    document.getElementById("registerFormContainer").classList.remove("d-none");
+});
+
+// Mostrar el formulario de inicio de sesión y ocultar el de registro
+document.getElementById("hideRegisterForm").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("registerFormContainer").classList.add("d-none");
+    document.getElementById("loginFormContainer").classList.remove("d-none");
+});
